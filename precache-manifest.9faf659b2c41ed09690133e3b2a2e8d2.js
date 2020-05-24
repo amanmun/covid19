c69e9a92a1524e42916d63b94c693d010c60947c
Aman Mundhra <amanmundhra12@gmail.com>
2020-05-24 13:36:57 +0530

    Updates

---
+++ precache-manifest.9faf659b2c41ed09690133e3b2a2e8d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a4a4110c11968da104c995f51480aa",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "730ed2665214820021ac",
+    "url": "/covid19/static/css/main.3119145b.chunk.css"
+  },
+  {
+    "revision": "b9eb25f1cc3c95f6e7d2",
+    "url": "/covid19/static/js/2.5671e5a4.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/covid19/static/js/2.5671e5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730ed2665214820021ac",
+    "url": "/covid19/static/js/main.76dd2392.chunk.js"
+  },
+  {
+    "revision": "3ee542c40c504318e94c",
+    "url": "/covid19/static/js/runtime-main.25158d71.js"
+  }
+]);
